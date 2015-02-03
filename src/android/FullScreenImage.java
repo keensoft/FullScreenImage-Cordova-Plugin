34f2f18b88e745bf35050b05554d740617d2bbfa
Alberto Munoz Fuertes <slaptot@gmail.com>
2015-02-03 14:09:17 +0100

    Android - Base64 working with png files

--- src/android/FullScreenImage.java
+++ src/android/FullScreenImage.java
@@ -10,6 +10,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 
 import android.annotation.SuppressLint;
 
@@ -22,6 +24,10 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Environment;
 import android.util.Log;
+import android.util.Base64;
+import android.*;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -30,32 +36,68 @@ import org.apache.cordova.CordovaPlugin;
 
 @SuppressLint("DefaultLocale")
 public class FullScreenImage extends CordovaPlugin {
+    private CallbackContext command;
     
-	@Override
-	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-		JSONObject json = args.getJSONObject(0);
-		String url = getJSONProperty(json, "url");
+    
+    /**
+     * Executes the request.
+     *
+     * This method is called from the WebView thread.
+     * To do a non-trivial amount of work, use:
+     *     cordova.getThreadPool().execute(runnable);
+     *
+     * To run on the UI thread, use:
+     *     cordova.getActivity().runOnUiThread(runnable);
+     *
+     * @param action   The action to execute.
+     * @param args     The exec() arguments in JSON form.
+     * @param callback The callback context used when calling
+     *                 back into JavaScript.
+     * @return         Whether the action was valid.
+     */
+    @Override
+    public boolean execute (String action, JSONArray args,
+                            CallbackContext callback) throws JSONException {
         
-		try {
-			doSendIntent(url);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return true;
-	}
+        this.command = callback;
+        
+        if ("showImageURL".equals(action)) {
+            showImageURL(args);
+            
+            return true;
+        }
+        
+        if ("showImageBase64".equals(action)) {
+            showImageBase64(args);
+            
+            return true;
+        }
+        
+        // Returning false results in a "MethodNotFound" error.
+        return false;
+    }
     
-	private String getJSONProperty(JSONObject json, String property) throws JSONException {
-		if (json.has(property)) {
-			return json.getString(property);
-		}
-		return null;
-	}
+    private String getJSONProperty(JSONObject json, String property) throws JSONException {
+        if (json.has(property)) {
+            return json.getString(property);
+        }
+        return null;
+    }
     
-	private void doSendIntent(String url) throws Exception {
-		String filenameArray[] = url.split("\\.");
-	    String extension = filenameArray[filenameArray.length-1];
-		InputStream inputStream = null;
-		OutputStream outputStream = null;
+    /**
+     * Show image in full screen from local resources.
+     *
+     * @param url     File path in local system
+     */
+    public void showImageURL (JSONArray args) throws JSONException {
+        JSONObject json = args.getJSONObject(0);
+        String url = getJSONProperty(json, "url");
+        
+        String filenameArray[] = url.split("\\.");
+        String extension = filenameArray[filenameArray.length-1];
+        
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
         
         File pPath= Environment.getExternalStorageDirectory();
         
@@ -69,6 +111,7 @@ public class FullScreenImage extends CordovaPlugin {
             outputStream =new FileOutputStream(f);
             byte buf[]=new byte[1024];
             int len;
+            
             while((len=inputStream.read(buf))>0)
                 outputStream.write(buf,0,len);
             outputStream.close();
@@ -84,8 +127,47 @@ public class FullScreenImage extends CordovaPlugin {
             Log.d("FullScreenImagePlugin", "Could not create file: " + e.toString());
             
         }
+    }
+    
+    
+    /**
+     * Show image in full screen from base64 String.
+     * @param base64       Image base64 String
+     * @param name              image Name to show on intent view
+     */
+    public void showImageBase64 (JSONArray args) throws JSONException{
+        JSONObject json = args.getJSONObject(0);
+        
+        String base64Image = getJSONProperty(json, "base64");
+        String name = getJSONProperty(json, "name");
+        File pPath= Environment.getExternalStorageDirectory();
+        if(!pPath.exists()) {
+            boolean bReturn= pPath.mkdirs();
+        }
+        
+        try {
+            String filetype = base64Image.substring(base64Image.indexOf(":")+1, base64Image.lastIndexOf(";"));
+            filetype = filetype.substring(filetype.indexOf("/")+1, filetype.length());
+            byte[] imageAsBytes = Base64.decode(base64Image.toString(), 0);
+            
+            File filePath= new File(pPath, "output."+filetype);
+            filePath.createNewFile();
+            
+            FileOutputStream os = new FileOutputStream(filePath, true);
+            os.write(imageAsBytes);
+            os.flush();
+            os.close();
+            
+            Uri path = Uri.fromFile(filePath);
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.setDataAndType(path, "image/*");
+            this.cordova.getActivity().startActivity(intent);
+            
+        } catch (IOException e) {
+            Log.d("FullScreenImagePlugin", "Could not create file: " + e.toString());
+        }
         
         
     }
-	
 }
