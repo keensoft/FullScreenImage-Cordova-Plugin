1138923d65f8f5cd23a91fbd7c1811432e9d3d58
Alberto Munoz Fuertes <slaptot@gmail.com>
2015-02-26 08:36:05 +0100

    - Fixed Cordova plugin executed in background mode. - New Example with shouldAutorotate, problem when device rotate on preview mode. - Init Development for array images (not implemented).

--- src/ios/FullScreenImage.m
+++ src/ios/FullScreenImage.m
@@ -11,58 +11,103 @@
 @implementation FullScreenImage
 
 
-- (void) showImageURL:(CDVInvokedUrlCommand*)command{
-    
-    NSString *fullPath = [[command.arguments objectAtIndex:0] valueForKey:@"url"];
-
-    NSString *soundFilePath = [NSString stringWithFormat:@"%@/www/%@",[[NSBundle mainBundle] resourcePath],fullPath];
-    NSURL *URL = [NSURL fileURLWithPath:soundFilePath];
-    
-    if (URL) {
-        UIDocumentInteractionController *documentInteractionController = [UIDocumentInteractionController interactionControllerWithURL:URL];
-        documentInteractionController.delegate = self;
-        BOOL menuOpenened = [documentInteractionController presentPreviewAnimated:YES];
-        if (!menuOpenened)
-            NSLog(@"Failed to open document in Document Directory");
+- (void)setupDocumentControllerWithURL:(NSURL *)url
+{
+    //checks if docInteractionController has been initialized with the URL
+    if (self.docInteractionController == nil)
+    {
+        self.docInteractionController = [UIDocumentInteractionController interactionControllerWithURL:url];
+        self.docInteractionController.delegate = self;
     }
-    
+    else
+    {
+        self.docInteractionController.URL = url;
+    }
+}
+
+
+- (void) showImageURL:(CDVInvokedUrlCommand*)command{
+    // Check command.arguments here.
+    [self.commandDelegate runInBackground:^{
+        self.documentURLs = [NSMutableArray array];
+        
+        
+        NSString *fullPath = [[command.arguments objectAtIndex:0] valueForKey:@"url"];
+        
+        NSString *soundFilePath = [NSString stringWithFormat:@"%@/www/%@",[[NSBundle mainBundle] resourcePath],fullPath];
+        NSURL *URL = [NSURL fileURLWithPath:soundFilePath];
+        
+        if (URL) {
+            
+            [self.documentURLs addObject:URL];
+            [self setupDocumentControllerWithURL:URL];
+            double delayInSeconds = 0.1;
+            dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+            dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+                [self.docInteractionController presentPreviewAnimated:YES];
+            });
+            
+            
+            
+        }
+    }];
 }
 
 - (void) showImageBase64:(CDVInvokedUrlCommand*)command{
+    
+    [self.commandDelegate runInBackground:^{
+        NSString *fullPath = [[command.arguments objectAtIndex:0] valueForKey:@"base64"];
+        
+        NSString *imageName = [[command.arguments objectAtIndex:0] valueForKey:@"name"];
+        
+        NSString *imageType = [[command.arguments objectAtIndex:0] valueForKey:@"type"];
+        
+        if([imageName isEqualToString:@""]){
+            imageName = @"default";
+        }
+        
+        NSString *str = [NSString stringWithFormat:@"data:image/@%;base64,",imageType];
+        str = [str stringByAppendingString:fullPath];
+        NSURL *url = [NSURL URLWithString:str];
+        NSData *imageData = [NSData dataWithContentsOfURL:url];
+        UIImage *ret = [UIImage imageWithData:imageData];
+        
+        
+        NSData *imageDataSaved=UIImagePNGRepresentation(ret);
+        
+        
+        NSString *docsDir;
+        NSArray *dirPaths;
+        
+        
+        dirPaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+        docsDir = [dirPaths objectAtIndex:0];
+        NSString *completeImageName = [NSString stringWithFormat:@"%@.%@",imageName,imageType];
+        NSString *databasePath = [[NSString alloc] initWithString: [docsDir stringByAppendingPathComponent:completeImageName]];
+        [imageDataSaved writeToFile:databasePath atomically:YES];
+        
+        NSURL *imageURL=[NSURL fileURLWithPath:databasePath];
+        
+        if (imageURL) {
+            [self.documentURLs addObject:imageURL];
+            [self setupDocumentControllerWithURL:imageURL];
+            double delayInSeconds = 0.1;
+            dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+            dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+                [self.docInteractionController presentPreviewAnimated:YES];
+            });
+        }
+    }];
+}
 
-    NSString *fullPath = [[command.arguments objectAtIndex:0] valueForKey:@"base64"];
+- (UIDocumentInteractionController *) setupControllerWithURL: (NSURL*) fileURL
+                                               usingDelegate: (id <UIDocumentInteractionControllerDelegate>) interactionDelegate {
     
-    NSString *imageName = [[command.arguments objectAtIndex:0] valueForKey:@"name"];
+    UIDocumentInteractionController *interactionController =
+    [UIDocumentInteractionController interactionControllerWithURL: fileURL];
+    interactionController.delegate = interactionDelegate;
     
-    
-    NSURL *url = [NSURL URLWithString:fullPath];
-    NSData *imageData = [NSData dataWithContentsOfURL:url];
-    UIImage *ret = [UIImage imageWithData:imageData];
-    
-    
-    NSData *imageDataSaved=UIImagePNGRepresentation(ret);
-    
-    
-    NSString *docsDir;
-    NSArray *dirPaths;
-    
-    
-    dirPaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-    docsDir = [dirPaths objectAtIndex:0];
-    NSString *completeImageName = [NSString stringWithFormat:@"%@.%@",imageName,[self contentTypeForImageData:imageDataSaved]];
-    NSString *databasePath = [[NSString alloc] initWithString: [docsDir stringByAppendingPathComponent:completeImageName]];
-    [imageDataSaved writeToFile:databasePath atomically:YES];
-    
-    NSURL *imageURL=[NSURL fileURLWithPath:databasePath];
-    
-    if (imageURL) {
-        UIDocumentInteractionController *documentInteractionController = [UIDocumentInteractionController interactionControllerWithURL:imageURL];
-        documentInteractionController.delegate = self;
-        BOOL menuOpenened = [documentInteractionController presentPreviewAnimated:YES];
-        if (!menuOpenened)
-            NSLog(@"Failed to open document in Document Directory");
-    }
-    
+    return interactionController;
 }
 
 - (UIViewController *) documentInteractionControllerViewControllerForPreview:(UIDocumentInteractionController *) controller {
